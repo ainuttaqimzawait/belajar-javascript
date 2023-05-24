6aa34dd0cff0c905dae923e4f6584fe04879b382
DESKTOP-H2UVHVF\PSK GAMING <ainuttaqimzawaid1000@gmail.com>
2023-05-24 08:20:13 +0700

    commit awal

---
+++ bassic.js
@@ -0,0 +1,25 @@
+JavaScript
+-membuat website lebih interaktif
+-
+2. Variable
+-var nama="zaw";    \
+-let nama="zaw";     |template literal
+-const nama="zaw";  /
+console.log('halo nama saya ${nama}');		sebelum ada template literal ->console.log("halo nama saya" + nama);
+perbedaan var dan let :var ruang lingkupnya window
+variable const tidak bisa diganti
+3. Tipe data
+-undefine
+	let nama;
+	console.log(nama);
+-string
+	let nama="zaw";
+	let nama='zaw';
+	console.log('halo nama saya ${nama}');
+-numbers->floating=koma2
+		  int 
+	let umur =23;
+	let tinggi = 170.5;
+	console.log('halo nama saya ${nama} dan umur saya ${umur}');
+-boolean
+
